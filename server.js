b72acf2fd32f452bcb40ff17e85b35ac9c0e159c
Ravi Maurya <ravi.maurya@quikr.com>
2022-04-10 12:40:08 +0530

    server.js is added for deployment

---
+++ server.js
@@ -0,0 +1,22 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router =jsonServer.router("./db.json");
+const middlewares = jsonServer.defaults({
+    static: "./build",
+});
+
+const port = 3001;
+server.use(middlewares);
+server.use(
+    jsonServer.rewriter(
+        {
+            "/api/*" : "/$1",
+        }
+    )
+);
+
+server.use(router);
+server.listen(port,() =>{
+    console.log(`Server is running on PORT ${port}`);
+}) ;
+
